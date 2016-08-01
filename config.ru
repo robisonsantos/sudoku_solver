a03d17bdda0994f82cee7f9396ef551b1b7b7690
Robison WR Santos <rwrsantos@gmail.com>
2016-07-31 18:58:33 -0700

    Barebones sudoku solver app

---
+++ config.ru
@@ -0,0 +1,2 @@
+require_relative 'sudoku_solver'
+run Sinatra::Application
---
+++ sudoku_solver.rb
@@ -0,0 +1,8 @@
+# This is the main controller for sudoku solver app
+
+require 'sinatra'
+
+get '/' do
+  erb :index
+end
+
